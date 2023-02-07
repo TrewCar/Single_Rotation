93f2601f1503634c4e79aadbaabe2a13d88ad0e9
TrewCar <113162792+TrewCar@users.noreply.github.com>
2023-02-07 21:51:58 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Timer = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(600, 600);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // Timer
+            // 
+            this.Timer.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(618, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(618, 103);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Reverse";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(618, 42);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Stop";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(619, 565);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(99, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "100";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(618, 549);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Интервал";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(615, 599);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "1000 = 1 сек ожидания";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(743, 620);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer Timer;
+    private System.Windows.Forms.Button button1;
+    private System.Windows.Forms.Button button2;
+    private System.Windows.Forms.Button button3;
+    private System.Windows.Forms.TextBox textBox1;
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.Label label2;
+}
+
+
---
+++ Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+public partial class Form1 : Form
+{
+    private int[,] Screen;
+
+    private Graphics g;
+
+    private int SZ = 10;
+
+    private int RectX;
+    private int RectY;
+
+    private int Shift = 0;
+    private bool rotate = true;
+    public Form1()
+    {
+        InitializeComponent();
+    }
+    private void Form1_Load(object sender, EventArgs e)
+    {
+        RectX = pictureBox1.Width / SZ;
+        RectY = pictureBox1.Height / SZ;
+
+        Screen = new int[RectX, RectY];
+
+        pictureBox1.Image = new Bitmap(Width, Height);
+
+        g = Graphics.FromImage(pictureBox1.Image);
+    }
+
+    private void timer1_Tick(object sender, EventArgs e)
+    {
+        for (int x = Shift; x < Screen.GetLength(0) - Shift; x += 2)
+        {
+            for (int y = Shift; y < Screen.GetLength(1) - Shift; y += 2)
+            {
+                int sum = Screen[x, y] + Screen[x + 1, y] + Screen[x, y + 1] + Screen[x + 1, y + 1];
+                if (sum != 1)
+                {
+                    continue;
+                }
+               
+                if(rotate == true)
+                {
+                    (Screen[x, y], Screen[x + 1, y], Screen[x + 1, y + 1], Screen[x, y + 1]) = 
+                        (Screen[x + 1, y], Screen[x + 1, y + 1], Screen[x, y + 1], Screen[x, y]);
+                }
+                else
+                {
+                    (Screen[x, y], Screen[x, y + 1], Screen[x + 1, y + 1], Screen[x + 1, y]) = 
+                        (Screen[x, y + 1], Screen[x + 1, y + 1], Screen[x + 1, y], Screen[x, y]);
+                    
+                }
+            }
+        }
+
+        for (int x = 0; x < Screen.GetLength(0); x++)
+        {
+            for (int y = 0; y < Screen.GetLength(1); y++)
+            {
+                g.FillRectangle(new SolidBrush(Screen[x, y] == 1 ? Color.Yellow : Color.Gray), x * SZ, y * SZ, SZ, SZ);
+            }
+        }
+
+        pictureBox1.Invalidate();
+        Shift = -Shift + 1;
+    }
+    private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+    {
+        if (e.X < 0 || e.Y < 0 || e.X > pictureBox1.Width || e.Y > pictureBox1.Height)
+            return;
+
+        if (e.Button == MouseButtons.Left)
+        {
+
+            int x = e.X / SZ;
+            int y = e.Y / SZ;
+
+            Screen[x, y] = 1;
+
+            g.FillRectangle(new SolidBrush(Color.Yellow), x * SZ, y * SZ, SZ, SZ);
+            pictureBox1.Invalidate();
+        }
+        if(e.Button == MouseButtons.Right)
+        {
+            int x = e.X / SZ;
+            int y = e.Y / SZ;
+
+            Screen[x, y] = 0;
+
+            g.FillRectangle(new SolidBrush(Color.Gray), x * SZ, y * SZ, SZ, SZ);
+            pictureBox1.Invalidate();
+        }
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+        Timer.Start();
+    }
+
+    private void button3_Click(object sender, EventArgs e)
+    {
+        Timer.Stop();
+    }
+
+    private void button2_Click(object sender, EventArgs e)
+    {
+        if (!Timer.Enabled)
+        {
+            Shift = -Shift + 1;
+            rotate = !rotate;
+        }
+        else
+        {
+            Timer.Stop();
+            Shift = -Shift + 1;
+            rotate = !rotate;
+            Timer.Start();
+        }
+    }
+
+    private void textBox1_TextChanged(object sender, EventArgs e)
+    {
+        try
+        { 
+            Timer.Interval = int.Parse(textBox1.Text); 
+        }
+        catch { }
+    }
+}
+
